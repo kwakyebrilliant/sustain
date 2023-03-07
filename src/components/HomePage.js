ec156ec7085cb79a9e48076d330345dd1365e678
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-07 15:06:23 +0000

    home updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import Navigation from './Navigation'
+import Navigation from './Navigation';
+import Footer from './Footer';
 
 import cover from '../assets/cover.jpg';
-import Footer from './Footer';
+import forest from '../assets/forest.jpeg';
+
 
 function HomePage() {
   return (
@@ -122,40 +124,130 @@ function HomePage() {
     
     <div className='relative px-20 grid grid-cols-1 lg:grid-cols-4 gap-x-8 gap-y-16'>
 
-    <a href="#" className="group relative block bg-black">
-  <img
-    alt="Developer"
-    src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
-    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
-  />
+    <a href="#singlepage" className="group relative block bg-black">
+        <img
+            alt="Developer"
+            src={forest}
+            className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+        />
 
-  <div className="relative p-4 sm:p-6 lg:p-8">
-    <p className="text-sm font-medium uppercase tracking-widest text-green-500">
-      Save Achimota Forest
-    </p>
+        <div className="relative p-4 sm:p-6 lg:p-8">
+            <p className="text-sm font-medium uppercase tracking-widest text-green-500">
+            Save Achimota Forest
+            </p>
 
-    <p className="text-xl font-bold text-white sm:text-2xl">
-        ETH 1/ $1,563.92
-    </p>
+            <p className="text-xl font-bold text-white sm:text-2xl">
+                ETH 1/ $1,563.92
+            </p>
 
-    <div className="mt-32 sm:mt-48 lg:mt-64">
-      <div
-        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
-      >
-        <p className="text-sm text-white">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-          perferendis hic asperiores quibusdam quidem voluptates doloremque
-          reiciendis nostrum harum. Repudiandae?
-        </p>
-      </div>
+            <div className="mt-32 sm:mt-48 lg:mt-64">
+            <div
+                className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+            >
+                <p className="text-sm text-white">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+                perferendis hic asperiores quibusdam quidem voluptates doloremque
+                reiciendis nostrum harum. Repudiandae?
+                </p>
+            </div>
+            </div>
+        </div>
+    </a>
+
+    <a href="#singlepage" className="group relative block bg-black">
+        <img
+            alt="Developer"
+            src={forest}
+            className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+        />
+
+        <div className="relative p-4 sm:p-6 lg:p-8">
+            <p className="text-sm font-medium uppercase tracking-widest text-green-500">
+            Save Achimota Forest
+            </p>
+
+            <p className="text-xl font-bold text-white sm:text-2xl">
+                ETH 1/ $1,563.92
+            </p>
+
+            <div className="mt-32 sm:mt-48 lg:mt-64">
+            <div
+                className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+            >
+                <p className="text-sm text-white">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+                perferendis hic asperiores quibusdam quidem voluptates doloremque
+                reiciendis nostrum harum. Repudiandae?
+                </p>
+            </div>
+            </div>
+        </div>
+    </a>
+
+    <a href="#singlepage" className="group relative block bg-black">
+        <img
+            alt="Developer"
+            src={forest}
+            className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+        />
+
+        <div className="relative p-4 sm:p-6 lg:p-8">
+            <p className="text-sm font-medium uppercase tracking-widest text-green-500">
+            Save Achimota Forest
+            </p>
+
+            <p className="text-xl font-bold text-white sm:text-2xl">
+                ETH 1/ $1,563.92
+            </p>
+
+            <div className="mt-32 sm:mt-48 lg:mt-64">
+            <div
+                className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+            >
+                <p className="text-sm text-white">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+                perferendis hic asperiores quibusdam quidem voluptates doloremque
+                reiciendis nostrum harum. Repudiandae?
+                </p>
+            </div>
+            </div>
+        </div>
+    </a>
+
+    <a href="#singlepage" className="group relative block bg-black">
+        <img
+            alt="Developer"
+            src={forest}
+            className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+        />
+
+        <div className="relative p-4 sm:p-6 lg:p-8">
+            <p className="text-sm font-medium uppercase tracking-widest text-green-500">
+            Save Achimota Forest
+            </p>
+
+            <p className="text-xl font-bold text-white sm:text-2xl">
+                ETH 1/ $1,563.92
+            </p>
+
+            <div className="mt-32 sm:mt-48 lg:mt-64">
+            <div
+                className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+            >
+                <p className="text-sm text-white">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+                perferendis hic asperiores quibusdam quidem voluptates doloremque
+                reiciendis nostrum harum. Repudiandae?
+                </p>
+            </div>
+            </div>
+        </div>
+    </a>
+
+
+
+
     </div>
-  </div>
-</a>
-
-
-
-
-</div>
 
     </div>
 
