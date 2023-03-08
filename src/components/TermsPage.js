c4b4367aa9ad22aebfa268acdf7b6c4ca5c25c86
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-08 18:49:51 +0000

    terms added

---
+++ src/components/TermsPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navigation from './Navigation';
+import Footer from './Footer';
+
+
+
+function TermsPage() {
+    return (
+      <div>
+          <Navigation />
+
+
+          <Footer />
+        </div>
+    )
+
+}
+
+export default TermsPage
