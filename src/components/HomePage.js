dccbbd58a85deddf91e6b2d0018d0d49da727148
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-08 19:43:30 +0000

    home updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -159,7 +159,7 @@ function HomePage() {
                     <div className="relative p-4 sm:p-6 lg:p-8">
 
                         <p className="text-sm font-medium capitalize tracking-widest text-white">
-                            Climate Action Targetting Air
+                            Climate Action Targeting Air
                         </p>
 
                         <p className="text-xl font-bold text-white sm:text-2xl">
