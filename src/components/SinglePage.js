2089c793a031992704d53a23612403bb8b350a41
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-08 12:15:10 +0000

    singlepage added

---
+++ src/components/SinglePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navigation from './Navigation';
+import Footer from './Footer';
+
+
+
+function SinglePage() {
+    return (
+      <div>
+          <Navigation />
+
+
+          <Footer />
+        </div>
+    )
+
+}
+
+export default SinglePage
