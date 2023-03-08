6d2111be1ec9d72f7f1e84d3883b9e19cc48ddf6
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-08 09:00:01 +0000

    action added

---
+++ src/components/ActionPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navigation from './Navigation';
+import Footer from './Footer';
+
+
+function ActionPage() {
+    return (
+      <div>
+          <Navigation />
+
+
+
+          <Footer />
+
+        </div>
+    )
+
+}
+
+export default ActionPage
