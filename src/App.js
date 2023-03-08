8826c4a30ad4734a778da82c2b784dabc2e37630
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-08 12:17:25 +0000

    app updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import ActionPage from "./components/ActionPage";
 import AboutPage from "./components/AboutPage";
+import SinglePage from "./components/SinglePage";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" exact element={<HomePage />} />
       <Route path="/action" exact element={<ActionPage />} />
       <Route path="/about" exact element={<AboutPage />} />
+      <Route path="/singlepage" exact element={<SinglePage />} />
       </Routes>
     </Router>
   );
