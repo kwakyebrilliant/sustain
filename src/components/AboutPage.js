9998f57b35c32fa9a4791a159f24370de534e487
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-08 09:13:21 +0000

    aboutpage added

---
+++ src/components/AboutPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navigation from './Navigation';
+import Footer from './Footer';
+
+
+
+function AboutPage() {
+    return (
+      <div>
+          <Navigation />
+
+          
+
+
+          <Footer />
+        </div>
+    )
+
+}
+
+export default AboutPage
