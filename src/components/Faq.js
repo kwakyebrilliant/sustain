f93a9c8d1ed7b5bf031cf40c5e9e3e9dd2e3c099
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-08 23:53:17 +0000

    faq updates

---
+++ src/components/Faq.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navigation from './Navigation';
+import Footer from './Footer';
+
+
+
+function FaqPage() {
+    return (
+      <div>
+          <Navigation />
+
+
+          <Footer />
+
+        </div>
+    )
+    }
+
+export default FaqPage
