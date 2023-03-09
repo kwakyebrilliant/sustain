9ab018b50a9603a8ce31af19a1118c26cf25c42e
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-09 22:06:32 +0000

    home updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -239,7 +239,11 @@ function HomePage() {
                 </p>
 
                 <p className="text-xl font-bold text-white sm:text-2xl">
-                    12/08/2021
+                    500 Tonnes CO<sub>2</sub>
+                </p>
+
+                <p className="text-sm text-white font-medium Capitalize tracking-widest">
+                To Offset
                 </p>
 
                 <div className="mt-32 sm:mt-48 lg:mt-64">
@@ -269,38 +273,11 @@ function HomePage() {
                 </p>
 
                 <p className="text-xl font-bold text-white sm:text-2xl">
-                    12/08/2021
+                    500 Tonnes CO<sub>2</sub>
                 </p>
 
-                <div className="mt-32 sm:mt-48 lg:mt-64">
-                <div
-                    className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
-                >
-                    <p className="text-sm text-white">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-                    perferendis hic asperiores quibusdam quidem voluptates doloremque
-                    reiciendis nostrum harum. Repudiandae?
-                    </p>
-                </div>
-                </div>
-            </div>
-        </a>
-
-
-        <a href="#singlepage" className="group relative block bg-black">
-            <img
-                alt="Developer"
-                src={forest}
-                className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
-            />
-
-            <div className="relative p-4 sm:p-6 lg:p-8">
-                <p className="text-sm font-medium uppercase tracking-widest text-green-500">
-                Achimota Forest
-                </p>
-
-                <p className="text-xl font-bold text-white sm:text-2xl">
-                    12/08/2021
+                <p className="text-sm text-white font-medium Capitalize tracking-widest">
+                To Offset
                 </p>
 
                 <div className="mt-32 sm:mt-48 lg:mt-64">
@@ -331,7 +308,46 @@ function HomePage() {
                 </p>
 
                 <p className="text-xl font-bold text-white sm:text-2xl">
-                    12/08/2021
+                    500 Tonnes CO<sub>2</sub>
+                </p>
+
+                <p className="text-sm text-white font-medium Capitalize tracking-widest">
+                To Offset
+                </p>
+
+                <div className="mt-32 sm:mt-48 lg:mt-64">
+                <div
+                    className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+                >
+                    <p className="text-sm text-white">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+                    perferendis hic asperiores quibusdam quidem voluptates doloremque
+                    reiciendis nostrum harum. Repudiandae?
+                    </p>
+                </div>
+                </div>
+            </div>
+        </a>
+
+
+        <a href="#singlepage" className="group relative block bg-black">
+            <img
+                alt="Developer"
+                src={forest}
+                className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+            />
+
+            <div className="relative p-4 sm:p-6 lg:p-8">
+                <p className="text-sm font-medium uppercase tracking-widest text-green-500">
+                Achimota Forest
+                </p>
+
+                <p className="text-xl font-bold text-white sm:text-2xl">
+                    500 Tonnes CO<sub>2</sub>
+                </p>
+
+                <p className="text-sm text-white font-medium Capitalize tracking-widest">
+                To Offset
                 </p>
 
                 <div className="mt-32 sm:mt-48 lg:mt-64">
