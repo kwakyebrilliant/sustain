12c2eb3dc14a33c5e185bf1c940a075b6251cdbb
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-09 21:43:48 +0000

    home page projects

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -220,7 +220,7 @@ function HomePage() {
         <div className='w-full pt-20'>
 
         <h1 className="flex py-5 lg:mx-20 md:mx-10 mx-5 font-bold text-3xl text-gray-800">
-            Available Data
+            Available Projects
         </h1>
 
 
