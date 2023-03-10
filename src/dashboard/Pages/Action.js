cc245e1e4f6a3fb277b085bfab304bd820cf8a18
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-10 18:42:55 +0000

    pages updates

--- src/dashboard/Pages/Action.js
+++ src/dashboard/Pages/Action.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import PartialNavbar from "../Partials/PartialNavbar";
+import Sidebar from '../Partials/Sidebar';
 
 function Action() {
   return (
-    <div>Actions</div>
+    <div className='text-black'>
+      <>
+        <div className='flex flex-auto'>
+          <Sidebar />
+          <div className='grow'>
+            <PartialNavbar />
+
+              Action
+
+          </div>
+        </div>
+      </>
+    </div>
   )
 }
 
