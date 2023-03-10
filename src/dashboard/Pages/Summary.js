abe3d35e6d74095d78538e3a192d9eddeb4122ae
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-10 18:41:43 +0000

    pages updates

--- src/dashboard/Pages/Summary.js
+++ src/dashboard/Pages/Summary.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import PartialNavbar from "../Partials/PartialNavbar";
+import Sidebar from '../Partials/Sidebar';
 
 function Summary() {
   return (
-    <div>Summary</div>
+    <div className='text-black'>
+      <>
+        <div className='flex flex-auto'>
+          <Sidebar />
+          <div className='grow'>
+            <PartialNavbar />
+
+              Summary
+
+          </div>
+        </div>
+      </>
+    </div>
   )
 }
 
