d21250c30901b6c0c07a0c3cc7c0dab6e141f20f
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-11 09:02:16 +0000

    action updates

--- src/dashboard/Pages/Action.js
+++ src/dashboard/Pages/Action.js
@@ -1,9 +1,57 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import PartialNavbar from "../Partials/PartialNavbar";
 import Sidebar from '../Partials/Sidebar';
 
+import { FaPlusCircle } from 'react-icons/fa'
+
+import { Web3Storage } from 'web3.storage';
+
+function getAccessToken () {
+    
+  return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDE0ZGU4NTUwMjAxMTdENDIyY0IxOTRBREJiZERlOTJGZjBkYzkxNzciLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjIyMDkwNzQ1NjUsIm5hbWUiOiJNb3ZlciJ9.04qLL5Jvrb2ZrrL3Mdpe-zMVmXVGuxroEAl_DwUT97E'
+}
+
+function makeStorageClient () {
+  return new Web3Storage({ token: getAccessToken() })
+}
+
 function Action() {
+
   const [showModal, setShowModal] = useState(false);
+  const [supportimage1, setSupportImage1] = useState(``);
+  const [supportimage2, setSupportImage2] = useState(``);
+
+  const hiddenFileSupport2 = useRef(null);
+
+    const handleClick4 = () => {
+      hiddenFileSupport2.current.click();
+    };
+
+    async function handleChange4(event) {
+      const supportUploaded2 = event.target.files[0];
+      setSupportImage2(URL.createObjectURL(event.target.files[0]));
+      const client = makeStorageClient()
+      const cid = await client.put([supportUploaded2])
+      console.log('stored files with cid:', cid)
+  
+      const res = await client.get(cid)
+      console.log(`Got a response! [${res.status}] ${res.statusText}`)
+      if (!res.ok) {
+        throw new Error(`failed to get ${cid} - [${res.status}] ${res.statusText}`)
+      }
+  
+  
+      const supports2 = await res.files();
+      setSupportImage2(`https://${cid}.ipfs.dweb.link/${supportUploaded2.name}`);
+      console.log(supportimage1)
+      console.log(supportUploaded2)
+      for (const file of supports2) {
+        console.log(`${file.cid} -- ${file.path} -- ${file.size}`)
+      }
+      return cid
+  
+    };
+
   return (
     <div className='text-black'>
       <>
@@ -75,7 +123,7 @@ function Action() {
                                     <div>
                                         <label className="sr-only" for="title">Title</label>
                                         <input
-                                        className="w-full rounded-lg border-gray-200 shadow-md p-3 text-sm"
+                                        className="w-full rounded-lg  text-black border border-gray-300 focus:outline-none focus:ring active:bg-gray-300 p-3 text-sm"
                                         placeholder="eg. Enter title"
                                         type="text"
                                         id="title"
@@ -89,7 +137,7 @@ function Action() {
                                     <div>
                                         <label className="sr-only" for="country">Country</label>
                                         <input
-                                        className="w-full rounded-lg border-gray-200 shadow-md p-3 text-sm"
+                                        className="w-full rounded-lg text-black border border-gray-300 focus:outline-none focus:ring active:bg-gray-300 p-3 text-sm"
                                         placeholder="eg. Enter country"
                                         type="text"
                                         id="country"
@@ -103,7 +151,7 @@ function Action() {
                                     <div>
                                         <label className="sr-only" for="description">Description</label>
                                         <textarea
-                                        className="w-full rounded-lg border-gray-200 shadow-md p-3 text-sm"
+                                        className="w-full rounded-lg text-black border border-gray-300 focus:outline-none focus:ring active:bg-gray-300 p-3 text-sm"
                                         placeholder="eg. give every other details"
                                         rows="5"
                                         id="description"
@@ -117,11 +165,37 @@ function Action() {
                                     <div>
                                         <label className="sr-only" for="amount">Amount</label>
                                         <input
-                                        className="w-full rounded-lg border-gray-200 shadow-md p-3 text-sm"
+                                        className="w-full rounded-lg text-black border border-gray-300 focus:outline-none focus:ring active:bg-gray-300 p-3 text-sm"
                                         placeholder="eg. 15 ETH"
                                         type="number"
                                         id="amount"
                                         />
+                                    </div>
+
+                                    <div className='mx-2 border-dotted border-2 border-green-700 justify-center'>
+                                        <div className='grid grid-rows-2 justify-center'>
+                                            <div onClick={handleClick4} className="inline-flex cursor-pointer items-center px-8 py-3 mt-8 text-white bg-green-600 border border-green-600 rounded hover:bg-transparent hover:text-green-600 active:text-green-500 focus:outline-none focus:ring">
+                                                <span className="text-sm font-medium">Upload Image </span>
+                                                <FaPlusCircle className='mx-2' />
+                                            </div>
+                                            <input type="file"
+                                                    ref={hiddenFileSupport2}
+                                                    onChange={handleChange4}
+                                                    style={{display:'none'}}
+                                                    accept=".png,.jpg,.jpeg"
+                                            /> 
+                                            <p className='m-2'>PNG, JPG, JPEG up to 5MB</p>
+                                            
+                                        </div>
+                                        {supportimage2 && (
+                                            <iframe
+                                            className='relative m-auto mb-3'
+                                                src={supportimage2}
+                                                accept=".png,.jpg,.jpeg"
+                                            >
+                                                </iframe>
+                                            )}
+                                      
                                     </div>
 
                                     <a className="group w-full relative inline-flex items-center overflow-hidden rounded bg-green-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-green-600" href="/">
@@ -141,7 +215,7 @@ function Action() {
                                             />
                                             </svg>
                                         </span>
-
+                                        
                                         <span className="text-sm font-medium transition-all group-hover:ml-4">
                                             Submit
                                         </span>
